ffeef568c7c30625a86bab9634b5c50e8d77d2f4
GabrielLaborda <gabriel.laborda.fsd@gmail.com>
2023-09-08 13:13:38 -0300

    Arreglo componente Home Cart

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import "./App.css";
 import ProductDetail from "./components/ProductDetail";
 import { Routes, Route } from "react-router-dom";
-
+import Home from './components/Home'
+import Cart from './components/Cart'
 function App() {
   return (
     <>
     <Routes>
-      <Route path='/Home' element={<Home />} />
+      <Route path='/' element={<Home />} />
       <Route path='/ProductDetail' element={<ProductDetail />} />
       <Route path='/Cart' element={<Cart />} />
     </Routes>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Home() {
   return (
-    <div><h1>Funciona</h1></div>
+    <div>Funciona</div>
   )
 }
 
