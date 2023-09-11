3f66c1e144174dcad6cbde4645135f39c088b015
GabrielLaborda <gabriel.laborda.fsd@gmail.com>
2023-09-11 19:11:07 -0300

    Featured y Category Diseño nuevo

--- src/components/CategoryHome.jsx
+++ src/components/CategoryHome.jsx
@@ -1,21 +1,50 @@
 import React from 'react'
+import "./CategoryHome.css"
 
 function CategoryHome() {
   return (
-    <div className="container">
-        <div className='row d-flex justify-content-center'>
-            <h3 className='text-center text-white my-5'>CATEGORY</h3>
-            <div className="col-6">
-                <img className='img-fluid mb-4' src="./ImgHome/category-completes-home-picture.webp" alt="" />
-                <img className='img-fluid' src="./ImgHome/category-decks-home-picture.webp" alt="" />
+        <div className="container-fluid">
+          <div className='row '>
+            <div className="col-6 bg-black d-flex flex-column justify-content-center align-items-center ">
+              <h2 className='categoryH2'>COMPLETES</h2>
+              <button className='btn btn-warning text-black fs-4 mt-5 rounded-0'> SHOP</button>
             </div>
-            <div className="col-6">
-                <img className='img-fluid mb-4' src="./ImgHome/category-trucks-home-picture.webp" alt="" />
-                <img className='img-fluid' src="./ImgHome/category-wheels-home-picture.webp" alt="" />
-            </div>            
+            <div className="col-6 p-0">
+              <img className='img-fluid w-100' src="./ImgHome/ImgCategoryCompletes.png" alt="" />
+            </div>
+          </div> 
+
+          <div className='row '>
+            <div className="col-6 bg-black d-flex flex-column justify-content-center align-items-center ">
+              <h2 className='categoryH2'>DECKS</h2>
+              <button className='btn btn-warning text-black fs-4 mt-5 rounded-0'> SHOP</button>
+            </div>
+            <div className="col-6 p-0">
+              <img className='img-fluid w-100' src="./ImgHome/ImgCategoryDecks.png" alt="" />
+            </div>
+          </div> 
+
+          <div className='row '>
+            <div className="col-6 bg-black d-flex flex-column justify-content-center align-items-center ">
+              <h2 className='categoryH2'>TRUCKS</h2>
+              <button className='btn btn-warning text-black fs-4 mt-5 rounded-0'> SHOP</button>
+            </div>
+            <div className="col-6 p-0">
+              <img className='img-fluid w-100' src="./ImgHome/ImgCategoryTrucks.png" alt="" />
+            </div>
+          </div> 
+
+          <div className='row '>
+            <div className="col-6 bg-black d-flex flex-column justify-content-center align-items-center ">
+              <h2 className='categoryH2'>WHEELS</h2>
+              <button className='btn btn-warning text-black fs-4 mt-5 rounded-0'> SHOP</button>
+            </div>
+            <div className="col-6 p-0">
+              <img className='img-fluid w-100' src="./ImgHome/ImgCategoryWheels.png" alt="" />
+            </div>
+          </div> 
+               
         </div>
-    </div>
-  )
-}
+)}
 
 export default CategoryHome
