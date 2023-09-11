859b099d550c4098004dc2ac3d1cda64afb09a23
soloSouza <134002966+soloSouza@users.noreply.github.com>
2023-09-11 18:55:39 -0300

    Subo lo que tengo hecho del about, hoy a la noche lo termino

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
-import "./App.css";
-import ProductDetail from "./components/ProductDetail";
-import { Routes, Route } from "react-router-dom";
-import Home from "./components/Home";
-import Cart from "./components/Cart";
+import './App.css';
+import ProductDetail from './components/ProductDetail';
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Cart from './components/Cart';
+import About from './components/About';
+
 function App() {
   return (
     <>
@@ -10,6 +12,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/ProductDetail" element={<ProductDetail />} />
         <Route path="/Cart" element={<Cart />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </>
   );
