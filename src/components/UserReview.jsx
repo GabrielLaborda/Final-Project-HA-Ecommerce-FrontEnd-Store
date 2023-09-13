62bf905fec169747ce218795018bbd46b0545131
ANDRESWEB <andresamorin1@gmail.com>
2023-09-13 16:01:10 -0300

    Cambio imagenes, Saco description hardcodeada

--- src/components/UserReview.jsx
+++ src/components/UserReview.jsx
@@ -1,4 +1,6 @@
 import ReactStars from "react-rating-stars-component";
+import UserAvatar from "../../public/User-avatar.png";
+import iconShare from "../../public/icons8-share.svg";
 
 function UserReview() {
 
@@ -10,7 +12,7 @@ function UserReview() {
   return (
     <div className="card d-flex flex-column userReview p-3 m-3 mb-5">
       <div className="d-flex align-items-center">
-        <img src="./User-avatar.png" alt="" className="rounded-circle me-2 userReviewImg"/>
+        <img src={UserAvatar} alt="" className="rounded-circle me-2 userReviewImg"/>
         <div className="d-flex justift-content-start w-100">
           <p className="userReviewTextName">User Name</p>
           <p className="userReviewTextVerified">Verified Buyer</p>
@@ -22,7 +24,7 @@ function UserReview() {
         <p className="userReviewTextTitle">Review Titlte</p>
         <p className="userReviewTextContent">Review Content...Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum sapiente quibusdam tenetur? Facilis in tempora obcaecati sint similique, rerum quasi placeat a blanditiis voluptatibus nostrum iusto rem cupiditate sequi quod explicabo animi quis omnis repellat quo? Provident perferendis eveniet voluptatem inventore consequatur aliquid delectus corporis.</p>
         <div className="d-flex">
-            <img src="./icons8-share.svg" alt="" className="userReviewIcon"/>
+            <img src={iconShare} alt="" className="userReviewIcon"/>
             <p className="userReviewTextShare">Share    |</p>
             <div className="d-flex userReviewTextShare ms-auto">
                 <p className="me-2">Was This Review Helpful?</p>
