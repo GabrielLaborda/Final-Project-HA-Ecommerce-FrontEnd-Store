6ee73ec48cd85cc1d5e1e4d46f0461929fac34a8
soloSouza <134002966+soloSouza@users.noreply.github.com>
2023-09-13 10:18:35 -0300

    Conectamos el carrousel de la home al backend

--- src/components/FeaturedHome.jsx
+++ src/components/FeaturedHome.jsx
@@ -1,145 +1,74 @@
-import React, { useRef, useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 import 'swiper/css';
 import { Navigation, Pagination } from 'swiper/modules';
-import './FeaturedHome.css'
-
+import './FeaturedHome.css';
+import axios from 'axios';
 
 function FeaturedHome() {
+  const baseURL = import.meta.env.VITE_API_BASE_URL;
+  const [products, setProducts] = useState(null);
+
+  const getProducts = async () => {
+    const response = await axios({
+      method: 'GET',
+      url: `${baseURL}/products`,
+      params: { featured: true },
+    });
+    setProducts(response.data);
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
   return (
     <>
-    
-    <div className="feacturedContainer">
-        <h3 className='feacturedH3 text-center'>FEATURED</h3>
+      <div className="feacturedContainer">
+        <h3 className="feacturedH3 text-center">FEATURED</h3>
         <Swiper
-             breakpoints={{
-                0: {
-                  slidesPerView: 1
-                },
-                576: {
-                  slidesPerView: 2
-                },
-                992: {
-                  slidesPerView: 4
-                },
-              }}
-            // slidesPerView={4}
-            spaceBetween={30}
-            loop={true}
-            pagination={{
-            clickable: true
-            }}
-            navigation={true}
-            modules={[Pagination, Navigation]}
-            className="mySwiper"
+          breakpoints={{
+            0: {
+              slidesPerView: 1,
+            },
+            576: {
+              slidesPerView: 2,
+            },
+            992: {
+              slidesPerView: 4,
+            },
+          }}
+          spaceBetween={30}
+          loop={true}
+          pagination={{
+            clickable: true,
+          }}
+          navigation={true}
+          modules={[Pagination, Navigation]}
+          className="mySwiper"
         >
-        <SwiperSlide>
-            <div className="swiper-slide p-1">
-                <div className= "item-swiper">
-                    <img className='img-fluid' src="./ImgHome/Features_1.webp" alt="" />
-                    <h6 className='my-3'>BAMBINO SKIPPER</h6>
-                    <p className=' mb-0'>USD 118.95</p>
+          {products &&
+            products.map((product) => (
+              <SwiperSlide key={product._id}>
+                <div className="swiper-slide p-1">
+                  <div className="item-swiper">
+                    <img
+                      className="img-fluid"
+                      src={`${baseURL}/img/${product.picture[0]}`}
+                      alt="Product Image"
+                    />
+                    <h6 className="my-3">{product.name}</h6>
+                    <p className=" mb-0">USD {product.price}</p>
+                  </div>
                 </div>
-            </div>
-        </SwiperSlide>
-        <SwiperSlide>
-            <div className="swiper-slide p-1">
-                <div className= "item-swiper">
-                    <img className='img-fluid' src="./ImgHome/Features_2.webp" alt="" />
-                    <h6 className='my-3 '>BAMBINO SHOREBREAK</h6>
-                    <p className=' mb-0'>USD 118.95</p>
-                </div>
-            </div>
-        </SwiperSlide>
-        <SwiperSlide>
-            <div className="swiper-slide p-1">
-                <div className= "item-swiper">
-                    <img className='img-fluid' src="./ImgHome/Features_3.webp" alt="" />
-                    <h6 className='my-3 '>BAMBINO BIVY</h6>
-                    <p className=' mb-0'>USD 118.95</p>
-                </div>
-            </div>
-        </SwiperSlide>
-        <SwiperSlide>
-            <div className="swiper-slide p-1">
-                <div className= "item-swiper">
-                    <img className='img-fluid' src="./ImgHome/Features_4.webp" alt="" />
-                    <h6 className='my-3 '>STRANDSQUAL</h6>
-                    <p className=' mb-0'>USD 118.95</p>
-                </div>
-            </div>
-        </SwiperSlide>
-        <SwiperSlide>
-            <div className="swiper-slide p-1">
-                <div className= "item-swiper">
-                    <img className='img-fluid' src="./ImgHome/Features_1.webp" alt="" />
-                    <h6 className='my-3 '>BAMBINO SKIPPER</h6>
-                    <p className=' mb-0'>USD 118.95</p>
-                </div>
-            </div>
-        </SwiperSlide>
-        <SwiperSlide>
-            <div className="swiper-slide p-1">
-                <div className= "item-swiper">
-                    <img className='img-fluid' src="./ImgHome/Features_2.webp" alt="" />
-                    <h6 className='my-3 '>BAMBINO SHOREBREAK</h6>
-                    <p className=' mb-0'>USD 118.95</p>
-                </div>
-            </div>
-        </SwiperSlide>
-        <SwiperSlide>
-            <div className="swiper-slide p-1">
-                <div className= "item-swiper">
-                    <img className='img-fluid' src="./ImgHome/Features_3.webp" alt="" />
-                    <h6 className='my-3 '>BAMBINO BIVY</h6>
-                    <p className=' mb-0'>USD 118.95</p>
-                </div>
-            </div>
-        </SwiperSlide>
-        <SwiperSlide>
-            <div className="swiper-slide p-1">
-                <div className= "item-swiper">
-                    <img className='img-fluid' src="./ImgHome/Features_4.webp" alt="" />
-                    <h6 className='my-3 '>STRANDSQUAL</h6>
-                    <p className=' mb-0'>USD 118.95</p>
-                </div>
-            </div>
-        </SwiperSlide>
-      </Swiper>
+              </SwiperSlide>
+            ))}
+        </Swiper>
       </div>
-  
     </>
-  )
+  );
 }
 
-export default FeaturedHome
-
-/*
-<div className="container-fluid feacturedContainer ">
-            <h3 className='feacturedH3 text-center mb-4 pt-5'>FEATURED</h3>
-            <div className="row">
-                <div className="d-flex flex-column align-items-center">
-                    <img className='img-fluid' src="./ImgHome/Features_1.webp" alt="" />
-                    <h6 className='my-3 '>BAMBINO SKIPPER</h6>
-                    <p className=' mb-4'>USD 118.95</p>
-                </div>
-                <div className="col-3 w-25 d-flex flex-column align-items-center">
-                    <img className='img-fluid' src="./ImgHome/Features_2.webp" alt="" />
-                    <h6 className='my-3 '>BAMBINO SHOREBREAK</h6>
-                    <p className=' mb-4'>USD 118.95</p>
-                </div>
-                <div className="col-3 w-25 d-flex flex-column align-items-center">
-                    <img className='img-fluid' src="./ImgHome/Features_3.webp" alt="" />
-                    <h6 className='my-3 '>BAMBINO BIVY</h6>
-                    <p className=' mb-4'>USD 118.95</p>
-                </div>
-                <div className="col-3 w-25 d-flex flex-column align-items-center">
-                    <img className='img-fluid' src="./ImgHome/Features_4.webp" alt="" />
-                    <h6 className='my-3 '>STRANDSQUAL</h6>
-                    <p className=' mb-5'>USD 118.95</p>
-                </div>
-            </div> 
-        </div>
-        */
+export default FeaturedHome;
