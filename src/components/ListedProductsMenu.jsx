0d3d1093dfc1803bd80088de65c7075aeccb1ff1
GonzaloCuello01 <gonzacuello-farq@hotmail.com>
2023-09-14 18:13:49 -0300

    actualización de listado de productos

--- src/components/ListedProductsMenu.jsx
+++ src/components/ListedProductsMenu.jsx
@@ -13,60 +13,27 @@ function ListedProductsMenu() {
   };
 
   return (
-    <div className="col-3 d-flex justify-content-start">
       <div className="w-100">
-        <ul className="ps-0">
+        <ul className='px-0'>
           <li className="fw-bold fs-3 p-0 text-black">SHOP</li>
-          <hr className="p-0 my-2" />
-          <li className="p-0 m-0">
-            <NavLink
-              className={"text-decoration-none"}
-              to={"/products"}
-              end
-              style={navLinkStyles}
-            >
-              ALL PRODUCTS
-            </NavLink>
-          </li>
-          <li className="p-0 m-0">
-            <NavLink
-              className={"text-decoration-none"}
-              to={"/products/completes"}
-              style={navLinkStyles}
-            >
-              COMPLETES
-            </NavLink>
-          </li>
-          <li className="p-0 m-0">
-            <NavLink
-              className={"text-decoration-none"}
-              to={"/products/decks"}
-              style={navLinkStyles}
-            >
-              DECKS
-            </NavLink>
-          </li>
-          <li className="p-0 m-0">
-            <NavLink
-              className={"text-decoration-none"}
-              to={"/products/trucks"}
-              style={navLinkStyles}
-            >
-              TRUCKS
-            </NavLink>
-          </li>
-          <li className="p-0 m-0">
-            <NavLink
-              className={"text-decoration-none"}
-              to={"/products/wheels"}
-              style={navLinkStyles}
-            >
-              WHEELS
-            </NavLink>
-          </li>
+          <hr className='p-0 my-2' />
+          <li className='p-0 m-0'><NavLink className={'text-decoration-none'} to={'/products/'} end style={navLinkStyles}>
+            ALL PRODUCTS
+          </NavLink></li>
+          <li className='p-0 m-0'><NavLink className={'text-decoration-none'} to={'/products/completes'} style={navLinkStyles}>
+            COMPLETES
+          </NavLink></li>
+          <li className='p-0 m-0'><NavLink className={'text-decoration-none'} to={'/products/decks'} style={navLinkStyles}>
+            DECKS
+          </NavLink></li>
+          <li className='p-0 m-0'><NavLink className={'text-decoration-none'} to={'/products/trucks'} style={navLinkStyles}>
+            TRUCKS
+          </NavLink></li>
+          <li className='p-0 m-0'><NavLink className={'text-decoration-none'} to={'/products/wheels'} style={navLinkStyles}>
+          WHEELS
+          </NavLink></li>
         </ul>
       </div>
-    </div>
   );
 }
 
--- src/components/ShopItemCard.jsx
+++ src/components/ShopItemCard.jsx
@@ -5,8 +5,8 @@ import "./ShopItemCard.css";
 function ShopItemCard({ name, picture, price, productSlug, categorySlug }) {
   const baseURL = import.meta.env.VITE_API_BASE_URL;
   return (
-    <div className="col">
-      <div className="cardContainer text-black d-flex justify-content-center px-4 m-2 h-100">
+    <div className="d-flex col-sm-12 col-md-6 col-lg-4 col-xl-3 h-100">
+      <div className='card border-0 mb-3 h-100'>
         <NavLink
           className={"text-black text-decoration-none"}
           to={`/products/${categorySlug}/${productSlug}`}
@@ -15,15 +15,13 @@ function ShopItemCard({ name, picture, price, productSlug, categorySlug }) {
             <img
               src={`${baseURL}/img/${picture}`}
               className="card-img"
-              alt="product-image"
-            />
+              alt="product-image"/>
             <div className="card-body text-black">
               <h1 className="card-title fs-5">{name}</h1>
               <p className="card-sub-title">USD {price}</p>
               <button
                 type="button"
-                className="btn btn-outline-dark rounded-0 w-100 border-secondary-subtle"
-              >
+                className="btn btn-outline-dark rounded-0 w-100 border-secondary-subtle">
                 Add to cart
               </button>
             </div>
