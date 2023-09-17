3a65469c5237ed213df1845971274da801cab1a4
soloSouza <134002966+soloSouza@users.noreply.github.com>
2023-09-17 13:33:49 -0300

    Order details

--- src/components/AccountOrders.jsx
+++ src/components/AccountOrders.jsx
@@ -3,55 +3,54 @@ import axios from 'axios';
 import { useSelector } from 'react-redux';
 import { useState, useEffect } from 'react';
 
-
-
 function AccountOrders() {
-const [orders, setOrders] = useState([]);
-const user = useSelector((state) => state.user);
-const baseURL = import.meta.env.VITE_API_BASE_URL;
+  const [orders, setOrders] = useState([]);
+  const user = useSelector((state) => state.user);
+  const baseURL = import.meta.env.VITE_API_BASE_URL;
 
-const getUser = async()=> {
-const response = await axios({
-    method: 'GET',
-    url: `${baseURL}/users/${user.id}`,
-    headers: {
+  const getUser = async () => {
+    const response = await axios({
+      method: 'GET',
+      url: `${baseURL}/users/${user.id}`,
+      headers: {
         Authorization: `Bearer ${user.token}`,
       },
-})
-setOrders(response.data.orders)
-console.log(response.data.orders)
-}
-useEffect(()=>{
-    getUser()
-},[])
+    });
+    setOrders(response.data.orders);
+    console.log(response.data.orders);
+  };
+  useEffect(() => {
+    getUser();
+  }, []);
 
-return (
-    (orders && 
-    <div>
+  return (
+    orders && (
+      <div>
         <Table striped bordered hover variant="light">
-            <thead>
-                <tr>
-                <th>Order Id</th>
-                <th>Date</th>
-                <th>Total</th>
-                <th>Status</th>
-                <th>Details</th>
-                </tr>
-            </thead>
-            {orders.map((order)=> {
-                <tbody>
-                    <tr>
-                    <td>{order._id}</td>
-                    <td>{order.updatedAt}</td>
-                    <td>{order.subtotal}</td>
-                    <td>{order.status}</td>
-                    <td>View Order</td>
-                </tr>
+          <thead>
+            <tr>
+              <th>Order Id</th>
+              <th>Date</th>
+              <th>Total</th>
+              <th>Status</th>
+              <th>Details</th>
+            </tr>
+          </thead>
+          {orders.map((order) => (
+            <tbody>
+              <tr>
+                <td>{order._id}</td>
+                <td>{order.updatedAt}</td>
+                <td>{order.subtotal}</td>
+                <td>{order.status}</td>
+                <td>View Order</td>
+              </tr>
             </tbody>
-            })}
-            </Table>
-    </div>)
-  )
+          ))}
+        </Table>
+      </div>
+    )
+  );
 }
 
-export default AccountOrders
+export default AccountOrders;
