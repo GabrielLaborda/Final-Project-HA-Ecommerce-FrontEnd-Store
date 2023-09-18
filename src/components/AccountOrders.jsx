4960ca1eab583a6f7f9a39bc6a257d178c1fd7d5
soloSouza <134002966+soloSouza@users.noreply.github.com>
2023-09-18 11:55:34 -0300

    Ajuste account order

--- src/components/AccountOrders.jsx
+++ src/components/AccountOrders.jsx
@@ -2,7 +2,7 @@ import Table from 'react-bootstrap/Table';
 import axios from 'axios';
 import { useSelector } from 'react-redux';
 import { useState, useEffect } from 'react';
-import "./AccountOrders.css"
+import './AccountOrders.css';
 import { NavLink } from 'react-router-dom';
 
 function AccountOrders() {
@@ -19,22 +19,17 @@ function AccountOrders() {
       },
     });
     setOrders(response.data.orders);
-    console.log(response.data.orders)
+    console.log(response.data.orders);
   };
   useEffect(() => {
     getUser();
   }, []);
 
-
   return (
     <div>
-      {orders.length === 0 &&     
-              <p className="account-data-p">
-                  You haven't placed any orders yet.
-              </p>
-      }
-      {orders.length > 0 &&
-        <div className='table-responsive'>
+      {orders.length === 0 && <p className="account-data-p">You haven't placed any orders yet.</p>}
+      {orders.length > 0 && (
+        <div className="table-responsive">
           <Table className="table table-bordered table-hover">
             <thead>
               <tr>
@@ -45,22 +40,24 @@ function AccountOrders() {
                 <th>Details</th>
               </tr>
             </thead>
-              <tbody>
-            {orders.map((order) => (
+            <tbody>
+              {orders.map((order) => (
                 <tr key={order._id}>
                   <td>{order._id}</td>
                   <td>{new Date(order.updatedAt).toLocaleDateString()}</td>
                   <td>{order.subtotal.toFixed(2)}</td>
                   <td>{order.status.status}</td>
-                  <td><NavLink to={`${baseURL}/orders/${order._id}`}>View Order</NavLink></td>
+                  <td>
+                    <NavLink to={`/orders/${order._id}`}>View Order</NavLink>
+                  </td>
                 </tr>
-            ))}
-              </tbody>
+              ))}
+            </tbody>
           </Table>
         </div>
-}          
-      </div>
-    );
+      )}
+    </div>
+  );
 }
 
 export default AccountOrders;
