438cd045244a7bca0a44790db965a2a0080bdb5f
soloSouza <134002966+soloSouza@users.noreply.github.com>
2023-09-19 12:11:56 -0300

    ajuste app

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,39 @@
-import "./App.css";
-import ProductDetail from "./components/ProductDetail";
-import { Routes, Route } from "react-router-dom";
-import Home from "./components/Home";
+import './App.css';
+import ProductDetail from './components/ProductDetail';
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
 
-import Footer from "./components/Footer";
-import About from "./components/About";
-import ProductCategories from "./components/ProductCategories";
-import Login from "./components/Login";
-import AllProducts from "./components/AllProducts";
-import Navbar from "./components/Navbar";
-import Account from "./components/Account";
-import Register from "./components/Register";
-import ShoppingCart from "./components/ShoppingCart";
-import Checkout from "./components/Checkout";
+import Footer from './components/Footer';
+import About from './components/About';
+import ProductCategories from './components/ProductCategories';
+import Login from './components/Login';
+import AllProducts from './components/AllProducts';
+import Navbar from './components/Navbar';
+import Account from './components/Account';
+import Register from './components/Register';
+import ShoppingCart from './components/ShoppingCart';
+import Checkout from './components/Checkout';
 
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import EditAccount from "./components/EditAcount";
-import OrderDetail from "./components/OrderDetail";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import EditAccount from './components/EditAcount';
+import OrderDetail from './components/OrderDetail';
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 
 function App() {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location.pathname]);
+
   return (
     <>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route
-          path="/products/:categorySlug/:productSlug"
-          element={<ProductDetail />}
-        />
+        <Route path="/products/:categorySlug/:productSlug" element={<ProductDetail />} />
         <Route path="/cart" element={<ShoppingCart />} />
         <Route path="/about" element={<About />} />
         <Route path="/products/:categorySlug" element={<ProductCategories />} />
@@ -38,7 +43,7 @@ function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/edit/:id" element={<EditAccount />} />
         <Route path="/checkout" element={<Checkout />} />
-        <Route path="/orders/:id" element={<OrderDetail />}/>
+        <Route path="/orders/:id" element={<OrderDetail />} />
       </Routes>
       <Footer />
       <ToastContainer />
