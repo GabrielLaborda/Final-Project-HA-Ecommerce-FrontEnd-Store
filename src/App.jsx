42ebb8cd856b21362e610f7603905aa925053ed1
soloSouza <134002966+soloSouza@users.noreply.github.com>
2023-09-19 13:08:09 -0300

    Arregle un duplicado en la app

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,6 @@ import './App.css';
 import ProductDetail from './components/ProductDetail';
 import { Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
-
-import Footer from './components/Footer';
-import About from './components/About';
-import ProductCategories from './components/ProductCategories';
-import Login from './components/Login';
-import AllProducts from './components/AllProducts';
-import Navbar from './components/Navbar';
-import Account from './components/Account';
-import Register from './components/Register';
-import ShoppingCart from './components/ShoppingCart';
-import Checkout from './components/Checkout';
-
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import EditAccount from './components/EditAcount';
